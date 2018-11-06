d70c1c8b003f2dc21bf9294910a5b8aad4220127
AngelaAngela <angieangerilli@gmail.com>
2018-11-05 23:09:14 -0500

    Update quotes.js

--- nodes/quotes.js
+++ nodes/quotes.js
@@ -1,4 +1,4 @@
-// arrray of quotes which must be exported
+
 const quotes = [ "Together we can change the world, just one random act of kindness at a time. Ron Hall", "Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you. Princess Diana",
 "You cannot shake hands with a clenched fist. Indira Gandhi",
 "Learning never exhausts the mind. Leonardo da Vinci",
