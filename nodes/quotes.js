f4e65523435c045a6c7d3dc5becf220fad5a5f6a
angie <angieangerilli@gmail.com>
2018-11-05 23:07:36 -0500

    nodelab

---
+++ nodes/quotes.js
@@ -0,0 +1,15 @@
+// arrray of quotes which must be exported
+const quotes = [ "Together we can change the world, just one random act of kindness at a time. Ron Hall", "Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you. Princess Diana",
+"You cannot shake hands with a clenched fist. Indira Gandhi",
+"Learning never exhausts the mind. Leonardo da Vinci",
+ "All that we see or seem is but a dream within a dream. Poe"
+];
+
+function random (){
+	return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+const randomQuotes = random;
+
+module.exports = {randomQuotes,
+};
